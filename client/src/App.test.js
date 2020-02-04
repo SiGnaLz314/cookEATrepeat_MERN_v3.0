d2730239e6dc5e87cc8b883aa3e9e9e3f6fc74bc
SiGnaLz <e3.141x@gmail.com>
2020-02-03 22:39:55 -0500

    Refactored App Test to use Enzyme.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+// import renderer from 'react-test-renderer';
 import App from './App';
 
-test('App snapshot test', () => {
-  const component = renderer.create(<App />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+import { shallow } from 'enzyme';
+
+describe('App Component Testing', () => {
+  const wrapper = shallow(<App />);
+
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
 });
+
+// test('App snapshot test', () => {
+//   const component = renderer.create(<App />);
+//   const tree = component.toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
